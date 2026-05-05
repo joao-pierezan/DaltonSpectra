8c1c5704c363462926fc4140614947adf133e4f2
joao-pierezan <boscattopierezanjoaovitor@gmail.com>
2026-05-05 12:34:17 -0300

    sd
    sdfsf

--- sam2/servidor.py
+++ sam2/servidor.py
@@ -165,4 +165,4 @@ async def analisar_clique(x: int = Form(...), y: int = Form(...), arquivo: Uploa
 
 # LIGA O SERVIDOR PARA O SITE CONSEGUIR CONVERSAR COM ELE
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
